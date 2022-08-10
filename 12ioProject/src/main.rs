15a77d5d8cd9ff62238b460f1b9e4db6bf79cd3e
playdog-io <cqdxcs3@163.com>
2022-08-10 17:40:04 +0800

    12.3

--- 12ioProject/src/main.rs
+++ 12ioProject/src/main.rs
@@ -1,19 +1,24 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use ioProject::Config;
 
 fn main() {
+    // --snip--
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
+    if let Err(e) = ioProject::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
 
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
